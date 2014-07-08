1859eddbd7df2dcc74de73ebc016806caf5c7218
Antoine Richard <richard.antoine@gmail.com>
2014-07-08 23:01:10 +0200

    changed log format

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 var express = require('express')
-	, morgan  = require('morgan')
-	,	favicon = require('serve-favicon')
+  , morgan  = require('morgan')
+  ,	favicon = require('serve-favicon')
   , app     = express()
   , port    = process.env.PORT || 3000;
 
-app.use(morgan({ format: 'dev', immediate: true }));
+app.use(morgan('short'));
 app.use(favicon(__dirname + '/../client/images/favicon.ico'));
 
 app.use(express.static('client'));
