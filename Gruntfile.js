ea33dc4140f90de6cb7455916d66d0dc57a40a1f
Antoine Richard <richard.antoine@gmail.com>
2014-07-08 21:58:05 +0200

    merged the package/build files of the 2 modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,14 @@ module.exports = function(grunt) {
 
     sass: {
       options: {
-        includePaths: ['bower_components/foundation/scss']
+        includePaths: ['client/bower_components/foundation/scss']
       },
       dist: {
         options: {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'client/css/app.css': 'client/scss/app.scss'
         }        
       }
     },
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
-        files: 'scss/**/*.scss',
+        files: 'client/scss/**/*.scss',
         tasks: ['sass']
       }
     }
