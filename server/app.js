ebd0527ac81f5f4dec1830ba47ad4020c007a20f
Antoine Richard <richard.antoine@gmail.com>
2014-07-09 22:55:12 +0200

    testing env

--- server/app.js
+++ server/app.js
@@ -7,10 +7,14 @@ var express = require('express')
 app.use(morgan('short'));
 app.use(favicon(__dirname + '/../client/src/images/favicon.ico'));
 
-if ('production' == process.env.NODE_ENV) {
-  app.use(express.static('client/dist'));
-} else {
+console.log('###');
+console.log(process.env.NODE_ENV);
+console.log('###');
+
+if ('development' == process.env.NODE_ENV) {
   app.use(express.static('client/src'));
+} else {
+  app.use(express.static('client/dist'));
 }
 
 app.listen(port, function() {
